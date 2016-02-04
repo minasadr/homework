8da2d24079fe1708315900aebe5fea43470eda4b
Mina Sadr <mina.sh.sadr@gmail.com>
2016-02-04 19:13:47 +0100

    added few functions

--- src/d/Points.kt
+++ src/d/Points.kt
@@ -26,7 +26,17 @@ class Point2(x: Int, y: Int) {
 
 class Point(val x: Int, val y: Int) {
 
-    override fun equals(other: Any?): Boolean{
+    /**
+     * Calculates the distance of the current point from the given point
+     */
+    fun distance(point: Point): Double = Math.sqrt((x - point.x) * (x - point.x) + 0.0 + (y - point.y) * (y - point.y))
+
+    /**
+     * Returns a new Point, which is the addition of the current point with the given point.
+     */
+    fun add(point: Point): Point = Point(x + point.x, y + point.y)
+
+    override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other?.javaClass != this.javaClass) return false
 
@@ -80,4 +90,27 @@ class PointsTest {
         // is the same as
         assertFalse(p1.identityEquals(p3))
     }
+
+    @Test
+    fun testDistance() {
+        val first = Point(0, 0)
+        val second = Point(0, 10)
+        val third = Point(10, 0)
+        val forth = Point(10, 10)
+
+        assertEquals(10.0, first.distance(second))
+        assertEquals(10.0, first.distance(third))
+        assertEquals(14.142135623730951, first.distance(forth))
+    }
+
+    @Test
+    fun testAdd() {
+        assertEquals(Point(0, 0), Point(1, 1).add(Point(-1, -1)))
+
+        val p1 = Point(3, 5)
+        val p2 = Point(7, 10)
+
+        assertEquals(Point(10, 15), p1.add(p2))
+    }
+
 }
